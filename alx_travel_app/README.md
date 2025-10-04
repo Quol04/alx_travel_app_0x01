ccadfc5000006ca0a49a5b05bac96b32f9b51671
quol <kuolamol77@gmail.com>
2025-10-04 12:21:54 +0300

    added the necessary packages

---
+++ alx_travel_app/README.md
@@ -0,0 +1,18 @@
+# alx_travel_app_0x01
+
+## Features
+- CRUD APIs for Listings and Bookings
+- RESTful endpoints under `/api/`
+- Interactive API documentation with Swagger
+
+## Endpoints
+- `/api/listings/`
+- `/api/bookings/`
+- `/swagger/` (Swagger UI)
+- `/redoc/` (ReDoc UI)
+
+## Setup
+```bash
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py runserver
--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
 
-# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing Listings.
+    Provides CRUD operations.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing Bookings.
+    Provides CRUD operations.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
